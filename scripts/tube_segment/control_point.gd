e8c59d4c5d0483ef58aaf02ae769a17ea6421a38
fhgaha <antsy1992@gmail.com>
2024-11-15 11:16:51 +0300

    you can notify if transform changed apparantely!

--- scripts/tube_segment/control_point.gd
+++ scripts/tube_segment/control_point.gd
@@ -1,9 +1,14 @@
+@tool
 class_name ControlPoint extends Node3D
 
 enum ControlPointState {None, Drag}
 
 var state = ControlPointState.None
 
-func _physics_process(delta: float) -> void:
-	
+func _ready():
+	set_notify_local_transform(true)
+
+func _notification(what: int) -> void:
+	if what == Node3D.NOTIFICATION_LOCAL_TRANSFORM_CHANGED:
+		prints(self.name, "Local transform has changed!")
 	pass
