5ce193e76fe93f808d4ecbb5ee7c6ff769b8e75e
fhgaha <antsy1992@gmail.com>
2024-12-20 04:24:33 +0300

    changed font to not bold. still unable to center tab content.

--- assets/themes/neumorphism_theme/styles/StyleBoxNeuNormal.gd
+++ assets/themes/neumorphism_theme/styles/StyleBoxNeuNormal.gd
@@ -1,19 +1,22 @@
 @tool
-class_name StyleBoxMulti extends StyleBox
+class_name StyleBoxNeuNormal extends StyleBox
 
-@export var style_boxes: Array[StyleBox] = []
+@export var style_boxes: Array[StyleBoxFlat] = []
 
 func _draw(canvas_item: RID, rect: Rect2) -> void:
-	for style_box: StyleBox in style_boxes:
+	for style_box: StyleBoxFlat in style_boxes:
 		if style_box:
 			# style_box.draw(canvas_item, rect)
 
 			# compensate shadows sticking out
-			# this makes it impossible to use margins to position internal elements though
+			# this breaks using margins to position internal elements though
 			var margined_rect:= Rect2(
 				rect.position.x + get_margin(SIDE_LEFT),
 				rect.position.y + get_margin(SIDE_TOP)/2,
 				rect.size.x 	- get_margin(SIDE_RIGHT),
 				rect.size.y 	- get_margin(SIDE_BOTTOM)
 			)
+			
 			style_box.draw(canvas_item, margined_rect)
+			
+	
--- assets/themes/neumorphism_theme/styles/StyleBoxNeuPressed.gd
+++ assets/themes/neumorphism_theme/styles/StyleBoxNeuPressed.gd
@@ -1,11 +1,12 @@
 @tool
-class_name StyleBoxLineEdit extends StyleBox
+class_name StyleBoxNeuPressed extends StyleBox
 
-@export var style_boxes: Array[StyleBox] = []
+@export var style_boxes: Array[StyleBoxFlat] = []
 
 func _draw(canvas_item: RID, rect: Rect2) -> void:
-	for style_box: StyleBox in style_boxes:
+	for style_box: StyleBoxFlat in style_boxes:
 		if style_box:
 			style_box.draw(canvas_item, rect)
 			# not compensating shadows sticking out here cause we are able 
 			# to use content margin to position the internal text this way
+	
