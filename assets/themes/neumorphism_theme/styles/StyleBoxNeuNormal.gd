e458a1967f2632e1f4ab1725bd4e38b34e3302ad
fhgaha <antsy1992@gmail.com>
2024-12-25 20:19:27 +0300

    looks like finally fixed tabs

--- assets/themes/neumorphism_theme/styles/StyleBoxNeuNormal.gd
+++ assets/themes/neumorphism_theme/styles/StyleBoxNeuNormal.gd
@@ -1,22 +1,23 @@
 @tool
 class_name StyleBoxNeuNormal extends StyleBox
 
+@export var compensate: bool = true
 @export var style_boxes: Array[StyleBoxFlat] = []
 
 func _draw(canvas_item: RID, rect: Rect2) -> void:
 	for style_box: StyleBoxFlat in style_boxes:
 		if style_box:
-			# style_box.draw(canvas_item, rect)
-
-			# compensate shadows sticking out
-			# this breaks using margins to position internal elements though
-			var margined_rect:= Rect2(
-				rect.position.x + get_margin(SIDE_LEFT),
-				rect.position.y + get_margin(SIDE_TOP)/2,
-				rect.size.x 	- get_margin(SIDE_RIGHT),
-				rect.size.y 	- get_margin(SIDE_BOTTOM)
-			)
+			var new_rect : Rect2
+			match compensate:
+				true:
+					# compensate shadows sticking out
+					new_rect= Rect2(
+						rect.position.x + get_margin(SIDE_LEFT)/2,
+						rect.position.y + get_margin(SIDE_TOP)/2,
+						rect.size.x 	- get_margin(SIDE_RIGHT),
+						rect.size.y 	- get_margin(SIDE_BOTTOM)
+					)
+				false:
+					new_rect = rect
 			
-			style_box.draw(canvas_item, margined_rect)
-			
-	
+			style_box.draw(canvas_item, new_rect)
